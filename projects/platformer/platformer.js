d720c506bbf9a0acfa82fc804d4dc6c6b4da1844
larenzeg <larenzegreen13@gmail.com>
2023-09-21 17:12:33 +0000

    added collectables

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,16 +43,16 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(1050, 650, 200, 20)
+    createPlatform(700, 550, 200, 20)
+    createPlatform(330, 450, 200, 20)
+    createPlatform(50, 350, 200, 20)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+     createCollectable("steve", 100, 300, 10, 1);
 
 
 
